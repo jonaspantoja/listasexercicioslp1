72fafdd781677c8b35c6b3ecc4784bbe0b8a66cf
jonaspantoja <91160866+jonaspantoja@users.noreply.github.com>
2025-07-01 14:25:26 -0300

    Add files via upload

---
+++ lista1/RESOLUÇÃO.py
@@ -0,0 +1,37 @@
+'''
+FATEC
+INTELIGÊNCIA ARTIFICIAL
+LABORATÓRIO DE PROGRAMAÇÃO
+LISTA DE EXERCÍCIOS 1
+JONAS FERNANDO PANTOJA
+'''
+
+# Com uso do interpretador Python, resolva os seguintes problemas. Faça uso do comando print:
+
+print("Com uso do interpretador Python, resolva os seguintes problemas. Faça uso do comando print:\n")
+
+print(" 1. 5 - 3 = ", 5-3,)
+print(" 2. 4 * 2 = ",4*2,)
+print(" 3. 7 + 3 = ", 7*3,)
+print(" 4. 10 - 4 = ", 10-4,)
+print(" 5. 6 * 5 = ", 6*5,)
+print(" 6. 15 % 4 = ", 15%4,)
+print(" 7. 8 + 2 + 5 = ", 8+2+5,)
+print(" 8. 20 - 6 - 3 = ", 20-6-3,)
+print(" 9. 9 * 3 * 2 = ", 9*3*2,)
+print(" 10. 18 % 7 = ", 18%7,)
+print(" 11. 12 + 4 - 2 = ", 12+4-2,)
+print(" 12. 5 * 3 - 8 = ",5 * 3 - 8,)
+print(" 13. 16 %5 + 2 = ", 16 %5 + 2,)
+print(" 14. 10 + 6 * 2 = ", 10 + 6 * 2,)
+print(" 15. 25 - 9 % 4 = ", 25 - 9 % 4,)
+print(" 16. 7 * 2 +3 - 5 = ",7 * 2 +3 - 5,)
+print(" 17. 50 % 9 = ",50 % 9,)
+print(" 18. 6 + 8 * 3 - 10 = ",6 + 8 * 3 - 10,)
+print(" 19. 14 - 5 + 7 % 5 = ",14 - 5 + 7 % 5,)
+print(" 20. 9 * 2 * 3 % 5 = ", 9 * 2 * 3 % 5,)
+print(" 21. 20 % 6 + 4 * 3 =", 20 % 6 + 4 * 3,)
+print(" 22. 11 + 4 - 2 * 3 =", 11 + 4 - 2 * 3,)
+
+
+    
